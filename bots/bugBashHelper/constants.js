9ccea27f6a8cee64eb3c0be77bbe3b57273d0e05
Maksym Mykhailenko <maxceem@gmail.com>
2020-03-10 12:07:41 +0800

    feat: use branch "CF" for Bug Bash

--- bots/bugBashHelper/constants.js
+++ bots/bugBashHelper/constants.js
@@ -4,7 +4,7 @@ const BOT_MENTION_REGEXP = new RegExp(`\\s*@${BOT_NAME}\\s+`, 'i')
 
 const BOT_INTENT_REGEXP = new RegExp(`\\s*@${BOT_NAME}[^\\n]*\\s+(assign|unassign|ready|help)[^\\n]*`, 'i')
 
-const BUG_BASH_LABEL = 'CF-20'
+const BUG_BASH_LABEL = 'CF'
 
 const LABEL = {
   OPEN_FOR_PICKUP: 'Open for Pickup',
